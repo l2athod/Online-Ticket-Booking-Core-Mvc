ab01fc59375cdce9010609fc3046622be0baf1ae
Rathod Harsh <rathodharsh1974@gmail.com>
2023-03-14 00:36:06 +0530

    day-13 end

--- OnlineTicketBooking/Data/ApplicationDbContext.cs
+++ OnlineTicketBooking/Data/ApplicationDbContext.cs
@@ -1,9 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OnlineTicketBooking.Models;
 
 namespace OnlineTicketBooking.Data
 {
     public class ApplicationDbContext: DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new { am.ActorId, am.MovieId });
+            modelBuilder.Entity<Actor_Movie>().HasOne(am => am.Movie).WithMany(am => am.Actor_Movies).HasForeignKey(am => am.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(am => am.Actor).WithMany(am => am.Actor_Movies).HasForeignKey(am => am.ActorId);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get;set; }
+        public DbSet<Actor_Movie> Actor_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
     }
 }
--- OnlineTicketBooking/Models/Actor.cs
+++ OnlineTicketBooking/Models/Actor.cs
@@ -9,5 +9,7 @@ namespace OnlineTicketBooking.Models
         public string ProfilePictureUrl { get; set; }
         public string FullName { get; set; }
         public string Bio { get; set; }
+        // Movie has many relationship with Actor_Movies
+        public List<Actor_Movie> Actor_Movies { get; set; } 
     }
 }
